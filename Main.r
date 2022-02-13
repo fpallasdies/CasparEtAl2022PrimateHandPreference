61ec1400bc9aa5e0b97616f40244521f2551b576
Fabian <fabianpallasdies@gmail.com>
2022-02-13 12:55:21 +0100

    Preprint Commit

---
+++ Main.r
@@ -0,0 +1,253 @@
+
+library(nlme)
+library(MuMIn)
+library(MASS)
+library(lme4)
+
+library(ggplot2)
+library(modelsummary)
+
+source("Preprocessing.r")
+
+############### 
+### ChiSquared Tests
+
+#Creating a combined table of literature Data and our own
+HIs<-sumtab$HI
+Clade<- sumtab$Clade
+TreeName<-sumtab$Treename
+characterization<- sumtab$Category
+Sex <- sumtab$Sex
+Age <- sumtab$Age
+Genus<-as.character(sumtab$Genus)
+for (i in 1:length(characterization)) {
+  if (characterization[i] == "left-handed") {
+    characterization[i]<- "left"
+  }
+  if (characterization[i] == "right-handed") {
+    characterization[i]<- "right"
+  } 
+}
+
+compiledData <- data.frame(HIs, Clade, TreeName, characterization, Genus, Sex, Age)
+
+
+compiledData$characterization <- as.factor(compiledData$characterization)
+
+levels(compiledData$characterization)
+
+compiledData$TreeName <- as.factor(compiledData$TreeName)
+compiledData$Genus <- as.factor(compiledData$Genus)
+
+
+
+
+
+
+# Chisquared and t-tests
+newworld <-subset(compiledData, compiledData$Clade == "Platyrrhini")
+oldworld <-subset(compiledData, compiledData$Clade == "Cercopithecoidea")
+homi <-subset(compiledData, compiledData$Clade == "Hominoidea")
+homiWOSap <-subset(compiledData, compiledData$Clade == "Hominoidea" & compiledData$Genus != "Homo")
+
+
+pval <- vector()
+ttestpval <- vector()
+names <- vector()
+clade <- vector()
+ambifreq <- vector()
+HImean <- vector()
+HIabs <- vector()
+
+for (i in 1:length(levels(compiledData$TreeName))) {
+  print(i)
+  tempsub <- subset(compiledData, compiledData$TreeName == levels(compiledData$TreeName)[i] )
+  names <- append(names, levels(compiledData$TreeName)[i])
+  clade <- append(clade, tempsub$Clade[1])
+  
+  
+  freq <-summary(tempsub$characterization)
+  
+
+  
+  HImean <- append(HImean, mean(tempsub$HI))
+  HIabs <- append(HIabs, mean(abs(tempsub$HI)))
+  
+  
+  if (tempsub$Clade[1] == "Platyrrhini") {
+    res <- chisq.test(freq, p=c(0.159574, 0.420213,0.420213))
+  }
+  if (tempsub$Clade[1] == "Cercopithecoidea") {
+    #res <- chisq.test(freq, p=c(0.2414634, 0.3792683,0.3792683))
+    res <- chisq.test(freq, p=c(0.219114, 0.390443,0.390443))
+  }
+  if (tempsub$Clade[1] == "Hominoidea") {
+    res <- chisq.test(freq, p=c(0.20041, 0.399795,0.399795))
+  }
+  
+  pval <- append(pval, res$p.value)
+  
+  if (!is.na(tempsub$HIs[1])) {
+    x <- t.test(tempsub$HIs)
+    ttestpval<- append(ttestpval, x$p.value )
+    
+  }
+  else{
+    ttestpval<- append(ttestpval, NA )
+  }
+  
+
+
+
+}
+chiresults <- data.frame(names,pval, ttestpval, clade, HImean, HIabs)
+
+
+hist(pval)
+
+hist(ttestpval)
+
+
+ks.test(pval,"punif",0,1)
+ks.test(ttestpval,"punif",0,1)
+
+
+
+#Chisquared tests on genus level
+pval <- vector()
+ttestpval <- vector()
+names <- vector()
+clade <- vector()
+ambifreq <- vector()
+HImean <- vector()
+HIabs <- vector()
+
+for (i in 1:length(levels(compiledData$Genus))) {
+  print(i)
+  tempsub <- subset(compiledData, compiledData$Genus == levels(compiledData$Genus)[i] )
+  names <- append(names, levels(compiledData$Genus)[i])
+  clade <- append(clade, tempsub$Clade[1])
+  
+  
+  freq <-summary(tempsub$characterization)
+  
+  
+  
+  HImean <- append(HImean, mean(tempsub$HI))
+  HIabs <- append(HIabs, mean(abs(tempsub$HI)))
+  
+  
+  if (tempsub$Clade[1] == "Platyrrhini") {
+    res <- chisq.test(freq, p=c(0.159574, 0.420213,0.420213))
+  }
+  if (tempsub$Clade[1] == "Cercopithecoidea") {
+    #res <- chisq.test(freq, p=c(0.2414634, 0.3792683,0.3792683))
+    res <- chisq.test(freq, p=c(0.219114, 0.390443,0.390443))
+  }
+  if (tempsub$Clade[1] == "Hominoidea") {
+    res <- chisq.test(freq, p=c(0.20041, 0.399795,0.399795))
+  }
+  
+  pval <- append(pval, res$p.value)
+  
+  if (!is.na(tempsub$HIs[1])) {
+    x <- t.test(tempsub$HIs)
+    ttestpval<- append(ttestpval, x$p.value )
+    
+  }
+  else{
+    ttestpval<- append(ttestpval, NA )
+  }
+  
+  
+  
+  
+}
+chiresults.genus <- data.frame(names,pval, ttestpval, clade, HImean, HIabs)
+
+
+
+
+
+
+############
+#Phylogenetic signal
+HIlist <- predictor.dir$HI
+names(HIlist) <- rownames(predictor.dir)
+phylosig(arbol.dir, HIlist, method = "lambda", test = TRUE)
+
+
+HIabslist <- predictor$HIabs
+names(HIabslist) <- rownames(predictor)
+
+phylosig(arbol, HIabslist, method = "lambda", test = TRUE)
+
+
+
+
+
+#################
+###Phylogenetic generalized least squares
+
+
+
+
+
+glsControl(maxIter = 100, msMaxIter = 100)
+
+#############
+#Lateralization Strength
+
+model.full<-gls(HIabs~log(female.endocranial.volume)+Ecology+Habitual.tool.use.in.wild.populations, data=predictor,correlation=corPagel(0.936419,arbol, form=~Name.in.tree, fixed=TRUE), method ="ML")
+
+
+dfirst<-dredge(model.full)
+dsec<-get.models(dfirst, subset=TRUE)
+dAbs<-model.avg(dsec, revised.var=TRUE)
+summary(dAbs)
+
+
+modelplot(dAbs) 
+
+
+###### Direction
+
+model.fulldir<-gls(HI~log(female.endocranial.volume)+Ecology+Habitual.tool.use.in.wild.populations, data=predictor.dir, correlation=corPagel(0,arbol.dir, form=~Name.in.tree, fixed=TRUE), method ="ML")
+
+
+dfirst.dir<-dredge(model.fulldir)
+dsec.dir<-get.models(dfirst.dir, subset=TRUE)
+ddir<-model.avg(dsec.dir, revised.var=TRUE)
+summary(ddir)
+
+
+modelplot(ddir)
+
+
+
+
+
+###### without Humans
+model.fulldir<-gls(HI~log(female.endocranial.volume)+Ecology+Habitual.tool.use.in.wild.populations, data=predictor.dir.nohumans, correlation=corPagel(0,arbol.dir.nohumans,  form=~Name.in.tree, fixed=TRUE), method ="ML")
+
+dfirst.dir<-dredge(model.fulldir)
+dsec.dir<-get.models(dfirst.dir, subset=TRUE)
+ddir.nohumans<-model.avg(dsec.dir, revised.var=TRUE)
+summary(ddir.nohumans)
+
+modelplot(ddir.nohumans)
+
+
+##########
+#GLM on insertions
+
+tabalt<- subset(tab, is.na(tab$Handed.in.with..)== FALSE)
+tabalt$HIInsert <- (tabalt$HIInsert +1) /2
+
+firstmodel<-glmer(HIInsert~ Handed.in.with..+(1 |Individual), data=tabalt, family=binomial )#, na.action="na.omit")
+
+summary(firstmodel)
+
+
+
+
---
+++ Preprocessing.r
@@ -0,0 +1,81 @@
+#write.csv(sumtab, "sumtab.csv", row.names = FALSE)
+sumtab <- read.csv("sumtab.csv", header = TRUE, sep = ',')
+litPopulation <- read.csv("LiteraturedataPopLevel.csv", header = TRUE, sep = '\t')
+
+litPopulation$NameInTree <- NA
+
+for (i in 1:length(litPopulation$Species)){
+  litPopulation$NameInTree[i] <- paste(litPopulation$Genus[i], litPopulation$Species[i], sep="_")
+} 
+
+
+
+
+
+
+predictor <- read.csv("predictortab.csv", header = TRUE, sep = ',', stringsAsFactors=TRUE, fileEncoding="UTF-8-BOM")
+nameintree <- predictor$Name.in.tree
+
+predictor <- read.csv("predictortab.csv", header = TRUE, sep = ',', stringsAsFactors=TRUE, row.names = 9, fileEncoding="UTF-8-BOM")
+predictor$Name.in.tree <- nameintree
+
+
+
+
+
+#Phylo Data
+
+library(ape)
+library(nlme)
+library(geiger)
+library(phytools)
+
+arbol <- read.nexus("consensusTree_10kTrees_laterality_strength.nex")
+obj <-name.check(arbol,predictor, data.names = predictor$Name.in.tree)
+
+
+
+data_with_names <- predictor$HI
+names(data_with_names) <- predictor$Name.in.tree
+
+
+
+arbol.dir<- read.nexus("consensusTree_10kTrees_laterality_direction.nex")
+
+predictor.dir<- predictor
+obj <-name.check(arbol.dir,predictor.dir, data.names = predictor.dir$Name.in.tree)
+obj
+predictor.dir <- subset(predictor.dir, predictor.dir$Name.in.tree %in% obj$data_not_tree == FALSE )
+name.check(arbol.dir,predictor.dir, data.names = predictor.dir$Name.in.tree)
+
+
+obj <-name.check(arbol,predictor.dir, data.names = predictor.dir$Name.in.tree)
+obj
+arbol.dir<-drop.tip(arbol, obj$tree_not_data)
+name.check(arbol.dir,predictor.dir, data.names = predictor.dir$Name.in.tree)
+
+bm<-corBrownian(1, arbol, form= ~Name.in.tree)
+bmdir<-corBrownian(1, arbol.dir, form= ~Name.in.tree)
+
+
+
+
+predictor.nohumans <- subset(predictor, Name.in.tree != "Homo_sapiens")
+obj <-name.check(arbol,predictor.nohumans, data.names = predictor.nohumans$Name.in.tree)
+obj
+arbol.nohumans<-drop.tip(arbol, obj$tree_not_data)
+name.check(arbol.nohumans,predictor.nohumans, data.names = predictor.nohumans$Name.in.tree)
+bmnohumans<-corBrownian(1, arbol.nohumans, form= ~Name.in.tree)
+
+
+predictor.dir.nohumans <- subset(predictor.dir, Name.in.tree != "Homo_sapiens")
+obj <-name.check(arbol.dir,predictor.dir.nohumans, data.names = predictor.dir.nohumans$Name.in.tree)
+obj
+arbol.dir.nohumans<-drop.tip(arbol.dir, obj$tree_not_data)
+name.check(arbol.dir.nohumans,predictor.dir.nohumans, data.names = predictor.dir.nohumans$Name.in.tree)
+bmdirnohumans<-corBrownian(1, arbol.dir.nohumans, form= ~Name.in.tree)
+
+
+
+tab <- read.csv("detailedtab.csv", header = TRUE, sep = ',', stringsAsFactors=TRUE)
+
